1c5d995165c91e974157a0823ad6ada13cbddbf5
Arvinth <arvinth8122@gmail.com>
2021-11-07 00:28:06 +0530

    spinner added

--- src/CarryForwarded.js
+++ src/CarryForwarded.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import {Badge,  Card } from "antd";
+import { Badge, Card } from "antd";
 import { useHistory } from "react-router-dom";
 import { Carousel } from 'antd'
 import './carry.css'
 import { Rate } from 'antd';
+import { Spin, Space } from 'antd';
 
 import combineddata from './imagesBackend'
 import NavBarForCarryForwardedPage from './NavBarForCarryForwardedPage';
@@ -23,66 +24,78 @@ function CarryForwarded(props) {
         // lineHeight: '160px',
         textAlign: 'center',
         background: '#fff',
-      };
+    };
     let historyCarried = useHistory();
     console.log(historyCarried)
-    var [hotelsBasedOnkeywordData , setHotelsBasedOnkeywordData] =useState([]);
+    var [hotelsBasedOnkeywordData, setHotelsBasedOnkeywordData] = useState([]);
 
     useEffect(() => {
         let backendData = props.all_data;
-       let filteredHotels =  backendData.map(o=>{
+        let filteredHotels = backendData.map(o => {
 
-            let searchedAvailabe = o.menu_available.non_veg.filter(e=>{
-                            
-                         return e.food_name.toLowerCase().includes(historyCarried.location.state.toLowerCase())
-                    
-                    });
-            
-            if(searchedAvailabe.length > 0){
-                    return {...o}
-            }
-            
-            
-            }).filter(e=>{return e!= undefined});
-            filteredHotels.forEach(element => {
-                element.imagePath = combineddata[Math.floor(Math.random() *18)]
+            let searchedAvailabe = o.menu_available.non_veg.filter(e => {
+
+                return e.food_name.toLowerCase().includes(historyCarried.location.state.toLowerCase())
+
             });
-            setHotelsBasedOnkeywordData(filteredHotels)
+
+            if (searchedAvailabe.length > 0) {
+                return { ...o }
+            }
+
+
+        }).filter(e => { return e != undefined });
+        filteredHotels.forEach(element => {
+            element.imagePath = combineddata[Math.floor(Math.random() * 18)]
+        });
+        setHotelsBasedOnkeywordData(filteredHotels)
     }, [])
 
 
     return (
-        <div className = "carryForwarded_main_container">
-            <NavBarForCarryForwardedPage all_data={props.all_data} />
-        <div className="totalcardscarry">
-            {hotelsBasedOnkeywordData.map(o => (
-                <div className="carryforwardcard">
-                    <Badge.Ribbon  text={(o.offer_available).split("-")[0] + " off"} placement="start">
-                        <Card
-                            hoverable
-                            style={{ width: 240 }}
-                            cover={<img alt="example" src={o.imagePath} />}
-                        >
-                            <div className="carryminutes"><Badge  count={Math.floor(Math.random() * 30) + " mins"} ></Badge></div>
-                            <div className="carrymeta">
-                            <Meta title={o.hotel_name} description={o.average_price_to_order} />
-                            <Meta  description={o.kind_of_food_available.split(",")[Math.floor(Math.random() * o.kind_of_food_available.split(",").length)-1]} />
-                            </div>
-                            <Rate disabled defaultValue={Math.floor(Math.random() *(5-2)+2)} />
-                            <Carousel autoplay effect="fade">
-                                <div>
-                                    <h3 style={contentStyle}>Follows all Max Safety measures to ensure your food is safe</h3>
-                                </div>
-                                <div>
-                                    <h3 style={contentStyle}>3775+ orders placed from here recently</h3>
-                                </div>
 
-                            </Carousel>
-                        </Card>
-                    </Badge.Ribbon>
+        <div>
+            {hotelsBasedOnkeywordData.length == 0 ? (
+                <div className="spinner">
+                <Space size="middle">
+                    <Spin size="large" />
+                </Space>
                 </div>
-            ))}
-            </div>
+            ) : (
+                <div className="carryForwarded_main_container">
+                    <NavBarForCarryForwardedPage all_data={props.all_data} />
+                    <div className="totalcardscarry">
+                        {hotelsBasedOnkeywordData.map(o => (
+                            <div className="carryforwardcard">
+                                <Badge.Ribbon text={(o.offer_available).split("-")[0] + " off"} placement="start">
+                                    <Card
+                                        hoverable
+                                        style={{ width: 240 }}
+                                        cover={<img alt="example" src={o.imagePath} />}
+                                    >
+                                        <div className="carryminutes"><Badge count={Math.floor(Math.random() * 30) + " mins"} ></Badge></div>
+                                        <div className="carrymeta">
+                                            <Meta title={o.hotel_name} description={o.average_price_to_order} />
+                                            <Meta description={o.kind_of_food_available.split(",")[Math.floor(Math.random() * o.kind_of_food_available.split(",").length) - 1]} />
+                                        </div>
+                                        <Rate disabled defaultValue={Math.floor(Math.random() * (5 - 2) + 2)} />
+                                        <Carousel autoplay effect="fade">
+                                            <div>
+                                                <h3 style={contentStyle}>Follows all Max Safety measures to ensure your food is safe</h3>
+                                            </div>
+                                            <div>
+                                                <h3 style={contentStyle}>3775+ orders placed from here recently</h3>
+                                            </div>
+
+                                        </Carousel>
+                                    </Card>
+                                </Badge.Ribbon>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+
         </div>
     );
 }
